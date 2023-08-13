1068c2f4cd9eebe86cb70b30471819ccbce9a128
Elena Ter-Akopova <testfrog@yandex.ru>
2023-08-13 23:23:20 +0300

    Добавлена файлы и тесты в бренчу high-level-step-objects

--- demoqa/pages/registration_page.py
+++ demoqa/pages/registration_page.py
@@ -1,60 +1,56 @@
 from selene import browser, be, have, command
-
-import tests
+from demoqa.data.users import User
 from demoqa.resources import path
 
 
 class RegistrationPage:
+
     def __init__(self):
-        self.registered_user_data = browser.element('.table').all('td').even
+        self.first_name = browser.element('#firstName')
+        self.last_name = browser.element('#lastName')
+        self.email = browser.element('#userEmail')
+        self.gender = browser.all('[name=gender]')
+        self.mobile_number = browser.element('#userNumber')
+        self.subjects = browser.element('#subjectsInput')
+        self.hobbies = browser.all('#hobbiesWrapper label')
+        self.picture = browser.element('#uploadPicture')
+        self.address = browser.element('#currentAddress')
+        self.submit = browser.element('#submit')
 
     def open(self):
         browser.open('https://demoqa.com/automation-practice-form/')
+        browser.execute_script('document.querySelector("#fixedban").remove()')
         browser.element('footer').execute_script('element.remove()')
-
-    def fill_first_name(self, value):
-        browser.element('#firstName').should(be.blank).type(value)
         return self
 
-    def fill_last_name(self, value):
-        browser.element('#lastName').should(be.blank).type(value)
+    def register(self, user: User):
+        self.first_name.type(user.first_name)
+        self.last_name.type(user.last_name)
+        self.email.type(user.email)
+        self.gender.element_by(have.value(user.gender)).element('..').click()
+        self.mobile_number.type(user.mobile_number)
+        self.fill_date_of_birth(user.date_of_birth)
+        self.subjects.type(user.subjects).press_enter()
+        self.hobbies.element_by(have.text(user.hobbies)).click()
+        self.picture.send_keys(path(user.picture))
+        self.address.type(user.address)
+        self.fill_state(user.state)
+        self.fill_city(user.city)
+        self.submit_form()
         return self
 
-    def fill_email(self, value):
-        browser.element('#userEmail').should(be.blank).type(value)
-        return self
-
-    def fill_gender(self, value):
-        browser.all('[name=gender]').element_by(have.value(value)).element(
-            '..').click()
-        return self
-
-    def fill_mobile_number(self, value):
-        browser.element('#userNumber').should(be.blank).type(value)
-        return self
-
-    def fill_date_of_birth(self, day, month, year):
+    def fill_date_of_birth(self, date):
+        year = date.year
+        month = date.month - 1
+        day = date.strftime('%d')
         browser.element('#dateOfBirthInput').click()
-        browser.element('.react-datepicker__month-select').type(month)
-        browser.element('.react-datepicker__year-select').type(year)
+        browser.element('.react-datepicker__year-select').click()
         browser.element(
-            f'.react-datepicker__day--00{day}:not(.react-datepicker__day--outside-month)').click()
-        return self
-
-    def fill_subjects(self, value):
-        browser.element('#subjectsInput').type(value).press_enter()
-        return self
-
-    def fill_hobbies(self, value):
-        browser.all('#hobbiesWrapper label').element_by(have.text(value)).click()
-        return self
-
-    def select_picture(self, value):
-        browser.element('#uploadPicture').send_keys(path(value))
-        return self
-
-    def fill_address(self, value):
-        browser.element('#currentAddress').should(be.blank).type(value)
+            f'.react-datepicker__year-select option[value="{year}"]').click()
+        browser.element('.react-datepicker__month-select').click()
+        browser.element(
+            f'.react-datepicker__month-select option[value="{month}"]').click()
+        browser.element(f'.react-datepicker__day--0{day}').click()
         return self
 
     def fill_state(self, value):
@@ -73,14 +69,23 @@ class RegistrationPage:
         browser.element('#submit').perform(command.js.click)
         return self
 
-    def should_have_text(self, value):
-        browser.element('.modal-header').should(
-            have.exact_text(value))
-        return self
+    def should_have_registered(self, user):
+        browser.element('.table').all('td').even.should(have.exact_texts(
+            f'{user.first_name} {user.last_name}',
+            f'{user.email}',
+            f'{user.gender}',
+            f'{user.mobile_number}',
+            '{0} {1},{2}'.format(user.date_of_birth.strftime("%d"),
+                                 user.date_of_birth.strftime("%B"),
+                                 user.date_of_birth.year),
+            f'{user.subjects}',
+            f'{user.hobbies}',
+            f'{user.picture}',
+            f'{user.address}',
+            f'{user.state} {user.city}'
+        ))
 
-    def close(self):
-        browser.element('#closeLargeModal').click()
-
-    def cleared_registration_form(self):
+    def close_modal(self):
+        browser.element('#closeLargeModal').should(have.exact_text('Close')).click()
         browser.element('.modal-dialog').should(be.absent)
         browser.element('#firstName').should(be.blank)
--- tests/test_demoqa_student_registration_form.py
+++ tests/test_demoqa_student_registration_form.py
@@ -1,48 +1,29 @@
-from selene import browser, be, have, command
+import datetime
 
+from demoqa.data.users import User
 from demoqa.pages.registration_page import RegistrationPage
 
-
 def test_for_registration_form_demoqa():
-    # GIVEN
     registration_page = RegistrationPage()
-    registration_page.open()
 
-    # WHEN
-    (registration_page
-    .fill_first_name('Angelina')
-    .fill_last_name('Jolie')
-    .fill_email('AngelinaJolie@email.ru')
-    .fill_gender('Female')
-    .fill_mobile_number('1234567890')
-    .fill_date_of_birth('4', 'June', '1975')
-    .fill_subjects('Biology')
-    .fill_hobbies('Sports')
-    .select_picture('Angelina_Jolie.jpg')
-    .fill_address('Los Angeles, Borogodskaya, 17')
-    .fill_state("NCR")
-    .fill_city("Delhi")
-    .submit_form()
-     )
-
-    # THEN
-    registration_page.should_have_text('Thanks for submitting the form')
-    registration_page.registered_user_data.should(
-        have.exact_texts(
-            'Angelina Jolie',
-            'AngelinaJolie@email.ru',
-            'Female',
-            '1234567890',
-            '04 June,1975',
-            'Biology',
-            'Sports',
-            'Angelina_Jolie.jpg',
-            'Los Angeles, Borogodskaya, 17',
-            'NCR Delhi')
+    Angelina = User(
+        first_name='Angelina',
+        last_name='Jolie',
+        email='AngelinaJolie@email.ru',
+        gender='Female',
+        mobile_number='1234567890',
+        date_of_birth=datetime.date(year=1975, month=5, day=4),
+        subjects='Biology',
+        hobbies='Sports',
+        picture='Angelina_Jolie.jpg',
+        address='Los Angeles, Borogodskaya, 17',
+        state="NCR",
+        city="Delhi"
     )
 
-    # WHEN
-    registration_page.close()
+    registration_page.open()
+    registration_page.register(Angelina)
+    registration_page.should_have_registered(Angelina)
 
-    # THEN
-    registration_page.cleared_registration_form()
+    registration_page.close_modal()
+
